2737e8e6f4eb68f7df812d19955bff505764c818
tu1511 <131641671+tu1511@users.noreply.github.com>
2024-02-01 22:27:14 +0700

    draw with me

--- script.js
+++ script.js
@@ -23,19 +23,30 @@ var colorPaint ='#000000';
 var size = 10;
 sizeEl.innerText = size;
 
-document.addEventListener('mousedown', function(e) {
-    pos1 = {
-        x: e.offsetX,
-        y: e.offsetY
-    }
-    isDrawing = true
-})
+// Sự kiện bắt đầu vẽ trên máy tính và màn hình cảm ứng
+canvas.addEventListener('mousedown', startDrawing);
+canvas.addEventListener('touchstart', startDrawing);
 
-document.addEventListener('mousemove', function(e) {
+function startDrawing(e) {
+    e.preventDefault();
+    pos1 = {
+        x: (e.type === 'mousedown') ? e.offsetX : e.touches[0].clientX,
+        y: (e.type === 'mousedown') ? e.offsetY : e.touches[0].clientY
+    }
+    isDrawing = true;
+}
+
+// Sự kiện di chuyển trên máy tính và màn hình cảm ứng
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('touchmove', draw);
+
+function draw(e) {
+    e.preventDefault();
+    var rect = canvas.getBoundingClientRect();
     if (isDrawing) {
         pos2 = {
-            x: e.offsetX,
-            y: e.offsetY
+            x: (e.type === 'mousemove') ? e.clientX - rect.left : e.touches[0].clientX - rect.left,
+            y: (e.type === 'mousemove') ? e.clientY - rect.top : e.touches[0].clientY - rect.top
         }
 
         // fill net ve
@@ -55,40 +66,46 @@ document.addEventListener('mousemove', function(e) {
         pos1.x = pos2.x;
         pos1.y = pos2.y;
     }
-})
+}
 
-document.addEventListener('mouseup', function(e) {
-    isDrawing = false
-})
+// Sự kiện kết thúc vẽ trên máy tính và màn hình cảm ứng
+canvas.addEventListener('mouseup', stopDrawing);
+canvas.addEventListener('mouseleave', stopDrawing);
+canvas.addEventListener('touchend', stopDrawing);
+canvas.addEventListener('touchcancel', stopDrawing);
 
+function stopDrawing(e) {
+    e.preventDefault();
+    isDrawing = false;
+}
 
 color.addEventListener('change', function(e) {
     colorPaint = e.target.value;
-})
+});
 
 eraser.addEventListener('click', function() {
     colorPaint = '#ffffff';
-})
+});
 
 decrease.addEventListener('click', function() {
     size -= 5;
     size = size > 5 ? size : 5;
     sizeEl.innerText = size;
-})
+});
 
 increase.addEventListener('click', function() {
     size += 5;
     size = size < 30 ? size : 30;
     sizeEl.innerText = size;
-})
-
+});
 
 clear.addEventListener('click', function() {
-    var canvasStats = canvas.getClientRects()[0];
-    ctx.clearRect(0, 0, canvasStats.width, canvasStats.height);
-})
+    //     var canvasStats = canvas.getClientRects()[0];
+    //     ctx.clearRect(0, 0, canvasStats.width, canvasStats.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
 
 save.addEventListener('click', function() {
     var output = canvas.toDataURL('image/png');
     save.setAttribute('href', output);
-})
+});
